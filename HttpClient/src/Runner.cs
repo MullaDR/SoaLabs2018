4eccabacab8e52b2492d36862ca4b3a1cfa03a64
MullaDR <dmullakhmetov@gmail.com>
2018-12-20 23:32:56 +0400

    The second lab.

---
+++ HttpClient/src/Runner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Labs.src
+{
+    public class Runner
+    {
+        static void Main(string[] args)
+        {
+            var port = Console.ReadLine();
+            var httpClient = new Labs.HttpClient.Core.Classes.HttpClient("http://91.245.227.5", port);
+            httpClient.GetData("study/Ping", "");
+            var data = httpClient.GetData("study/GetInputData", "");
+            
+            httpClient.PostData("WriteAnswer", Serialization.Serialization.Serialize("Json", data));
+        }
+    }
+}
---
+++ HttpClient/src/Serialization/Serialization.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Labs.src.Serialization.Core.Classes;
+using Labs.src.Serialization.Data;
+
+namespace Labs.src.Serialization
+{
+    static class Serialization
+    {
+        private static Output Transformation(Input inputData)
+        {
+            var sumResults = inputData.Sums.Sum() * inputData.K;
+            var mulResults = inputData.Muls.Aggregate((x, y) => x * y);
+            var sortedInputs = inputData.Sums.Concat(inputData.Muls.Select(e => (decimal)e)).OrderBy(e => e).ToArray();
+            return new Output()
+            {
+                SumResult = sumResults,
+                MulResult = mulResults,
+                SortedInputs = sortedInputs
+            };
+        }
+
+        public static string Serialize(string serializationType, string data)
+        {
+            var converter = new Converter();
+            var serializeData = converter.Converters[serializationType].Deserialize<Input>(data);
+            var output = Transformation(serializeData);
+            var deserializeData = converter.Converters[serializationType].Serialize(output);
+            return deserializeData;
+        }
+    }
+}
